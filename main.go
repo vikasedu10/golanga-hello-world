933613c4d757f68863e5ef258cd00126149778e8
Vikas Bahuguna <vikasedu10@gmail.com>
2022-09-06 09:45:00 +0530

    Add interactive message to ticket creation app

--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello DevOps world!! :)")
+	var appName = "Go Conference"
+	const totalConferenceTickets uint = 50
+	var remainingTickets = totalConferenceTickets
 
-	var isActive bool = true
-	fmt.Printf("Number of DevOps engineers are active and allrounder? - %T \n", isActive)
+	fmt.Printf("Welcome to %v booking application!\n", appName)
+	fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", totalConferenceTickets, remainingTickets)
+	fmt.Println("Book your tickets now.")
 
-	var smallVal uint8 = 255
-	fmt.Println(smallVal)
-	fmt.Printf("Variable type: %T \n", smallVal)
+	var username string;
+	var email string;
+	var userTickets uint;
 
-	// Default values and aliases
-	var message string;
-	fmt.Println(message)
-	var marks int;
-	fmt.Println(marks)
+	fmt.Printf("Enter your name to book tickets: ")
+	fmt.Scan(&username)
+
+	fmt.Printf("Enter no of tickets you want to book: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Enter your email address to book tickets: ")
+	fmt.Scan(&email)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thankyou %v for booking %v tickets. You will receive a confirmation e-mail at %v\n", username, userTickets, email)
+	fmt.Printf("%v tickets now remaining for %v!\n", remainingTickets, appName)
 }
